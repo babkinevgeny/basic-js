8e42d2ec0fb6597d78eee0ee988e33a0acb82433
babkinevgeny <babkinevgeny@gmail.com>
2020-09-30 15:59:11 +0300

    feat: solve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,41 @@
+
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if ( !arr.length ) {
+    return [];
+  }
+
+  const seqArr = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const prev = arr[i - 1];
+    const next = arr[i + 1];
+    const cur = arr[i];
+
+    if (seqArr.includes(cur)) {
+      continue;
+    }
+
+    if (prev === '--discard-next') {
+      continue;
+    }
+
+    if (prev === '--double-next') {
+      newArr.push(cur);
+    }
+
+    if (next === '--discard-prev') {
+      continue;
+    }
+
+    if (next === '--double-prev') {
+      newArr.push(cur);
+    }
+
+    newArr.push(cur);
+  }
+  return newArr;
 };
