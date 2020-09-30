0cb0ea080cfe0852a1aee2a7e2990e1aab1002c9
babkinevgeny <babkinevgeny@gmail.com>
2020-09-30 17:08:02 +0300

    feat: solve extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+function customToString(value) {
+  let result;
+  switch (value) {
+    case null:
+      result = 'null';
+      break;
+    case false:
+      result = 'false';
+      break;
+    case undefined:
+      result = '';
+      break;
+    default:
+      result = value.toString();
+  }
+
+  return result;
+}
+
+module.exports = function repeater(str, options) {
+  str = customToString(str);
+  options.addition = customToString(options.addition);
+  options.separator = options.separator ? options.separator : '+';
+  options.additionSeparator = options.additionSeparator ? options.additionSeparator : '|';
+  
+  let addition = '';
+  let additionArr = [];
+
+  if (options.additionRepeatTimes) {
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      additionArr.push(options.addition);
+    }
+
+    addition = additionArr.join(options.additionSeparator)
+  } else {
+    addition = options.addition
+  }
+
+  let result = '';
+  let strArr = [];
+
+  if (options.repeatTimes) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      strArr.push(str + addition);
+    }
+    result = strArr.join(options.separator)
+  } else {
+    result = str + addition;
+  }
+
+  return result;
 };
   
